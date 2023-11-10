eecfec451399e1906ed1cdcd0e3e6bc02a9e8053
Lucianojrd <112493714+Lucianojrd@users.noreply.github.com>
2023-11-10 20:06:00 -0300

    Update lrd_conexion.php

--- lrd_includes/lrd_conexion.php
+++ lrd_includes/lrd_conexion.php
@@ -1,8 +1,9 @@
+//el proyecto esta sin uso por lo que esta de baja momentaneamente
 <?php
 function lrd_conectar()
 {
     global $con;
-    $con = mysqli_connect("localhost", "id21454904_lrd", "Akatsuki12_", "id21454904_lrd_parcial");
+    $con = mysqli_connect("localhost", "id21454904_lrd", "...", "id21454904_lrd_parcial");
     if (mysqli_connect_errno()) {
         printf("Falló la conexión: %s\n", mysqli_connect_error());
         exit();
